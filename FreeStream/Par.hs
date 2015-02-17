da156ced1a113ee1ff8e632cf025f34ab617b08c
Gatlin Johnson <gatlin@niltag.net>
2015-02-17 15:19:19 -0600

    better comments

--- FreeStream/Par.hs
+++ FreeStream/Par.hs
@@ -16,12 +16,14 @@ import FreeStream.Util
 import Data.Foldable
 import Control.Monad.Trans.Class
 
+-- | Broadcast the values of a source 'Task' to 'Functor' of sink @Task@s
 broadcast :: (Functor f, Monad m)
           =>     Task a b m r
           -> f ( Task b c m r )
           -> f ( Task a c m r )
 broadcast src tsks = fmap (\t -> src >< t) tsks
 
+-- | Infix synonym for 'broadcast'
 (*<) :: (Functor f, Monad m)
      =>     Task a b m r
      -> f ( Task b c m r )
@@ -29,11 +31,13 @@ broadcast src tsks = fmap (\t -> src >< t) tsks
 (*<) = broadcast
 infixl 9 *<
 
+-- | Naively merges the values of some 'Functor' of source 'Task's
 merge :: ( Foldable t, Monad m )
       => t ( Task a c m s )
       -> Task a c m ()
 merge tasks = for (each tasks) (\t -> for t yield)
 
+-- | 'merge's a 'Functor' of source 'Task's and pipes ('><') to a sink @Task@
 (>*) :: (Foldable t, Monad m)
      => t ( Task a b m s )
      ->     Task b c m ()
