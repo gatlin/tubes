13499905a84df80272bd33d35f5dc3969573eec8
Gatlin Johnson <gatlin.johnson@ipsoft.com>
2015-06-01 10:24:52 -0500

    fixed another licensing typo

--- Tubes/Core.hs
+++ Tubes/Core.hs
@@ -3,7 +3,7 @@ Module          : Tubes.Core
 Description     : Fundamental types and operations.
 Copyright       : (c) 2014, 2015 Gatlin Johnson <gatlin@niltag.net>
 
-License         : LGPL-3
+License         : GPL-3
 Maintainer      : gatlin@niltag.net
 Stability       : experimental
 -}
