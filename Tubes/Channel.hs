3d355f0e82b57cda3da8aa55caa79b925fc1b1f1
Gatlin Johnson <gatlin@niltag.net>
2016-09-01 13:00:32 -0500

    ArrowChoice, Applicative, and Monad instances for Channel

--- Tubes/Channel.hs
+++ Tubes/Channel.hs
@@ -91,6 +91,31 @@ instance Monad m => Category (Channel m) where
     id  = Channel cat
     c_bc . c_ab = Channel $ forever (tune c_ab) >< forever (tune c_bc)
 
+instance Monad m => Functor (Channel m a) where
+    fmap = rmap
+
+instance Monad m => Applicative (Channel m a) where
+    pure x = Channel $ await >>= \_ -> yield x
+    ch1 <*> ch2 = ch1 &&& ch2 >>> arr (\(f,x) -> f x)
+
+-- Not sure how useful this is but whatever
+instance Monad m => Monad (Channel m a) where
+    return = pure
+    ma >>= f = _join (fmap f ma) where
+        _join (Channel tb) = Channel $ loop tb
+        loop tb = do
+            a <- await
+            mChB <- lift $ pass a tb
+            case mChB of
+                Nothing -> halt
+                Just ((Channel tbB), tb') -> do
+                    mB <- lift $ pass a tbB
+                    case mB of
+                        Nothing -> halt
+                        Just (b, _) -> do
+                            yield b
+                            loop tb'
+
 instance (Monad m) => Arrow (Channel m) where
     arr f = Channel $ map f
 
@@ -98,21 +123,40 @@ instance (Monad m) => Arrow (Channel m) where
         loop tb = do
             ~(b,d) <- await
             mr <- lift $ pass b tb
-            case mr of
-                Just (c,k) -> do
-                    yield (c, d)
-                    loop k
-                Nothing -> halt
+            maybe halt (\(c,k) -> (yield (c,d)) >> loop k) mr
 
     second ch = Channel $ loop (tune ch) where
         loop tb = do
             ~(d,b) <- await
             mr <- lift $ pass b tb
-            case mr of
-                Just (c,k) -> do
-                    yield (d, c)
-                    loop k
-                Nothing -> halt
+            maybe halt (\(c,k) -> (yield (d, c)) >> loop k) mr
+
+instance (Monad m) => ArrowChoice (Channel m) where
+    (Channel tb1) +++ (Channel tb2) = Channel $ loop tb1 tb2  where
+        loop t1 t2 = await >>=
+            either
+            (\l -> (lift $ pass l t1) >>=
+                maybe halt (\(out, t1') ->
+                                (yield $ Left out) >> loop t1' t2))
+            (\r -> (lift $ pass r t2) >>=
+                maybe halt (\(out, t2') ->
+                                (yield $ Right out) >> loop t1 t2'))
+
+    left (Channel tb) = Channel $ loop tb where
+        loop tb = await >>=
+            either
+            (\v -> (lift $ pass v tb) >>=
+                maybe halt (\(out, tb') ->
+                                (yield $ Left out) >> loop tb'))
+            (\v -> (yield $ Right v) >> loop tb)
+
+    right (Channel tb) = Channel $ loop tb where
+        loop tb = await >>=
+            either
+            (\v -> (yield $ Left v) >> loop tb)
+            (\v -> (lift $ pass v tb) >>=
+                maybe halt (\(out, tb') ->
+                                (yield $ Right out) >> loop tb'))
 
 {- |
 Convert a 'Sink m a' into a 'Channel m a a', re-forwarding values downstream.
